f340d9be5fd856d7a2669de8e5d8713ea8f04e4a
Лишина Любовь ТКИ-141 <lishina15luybov@gmail.com>
2025-11-17 10:21:16 +0300

    Update 1,2.c

--- 1,2.c
+++ 1,2.c
@@ -6,13 +6,13 @@
  * @param side радиус шара
  * @return возвращает рассчитанный объем
  */
-double getA (const double side);
+double getVolume (const double side);
 /**
  * @brief рассчитывает площадь поверхности шара
  * @param side радиус шара
  * @return возвращает рассчитанную площадь поверхности
  */
-double getB (const double side);
+double getSquare (const double side);
 /**
  * @brief точка входа в программу
  * @return возвращает 0, если программма выполнена корректно
@@ -22,16 +22,16 @@ int main(void)
     double side;
     scanf("%lf",&side);
     printf("радиус равен %.2lf\n", side);
-    printf("объем равен %.2lf",getA(side));
-    printf("площадь поверхности равна %.2lf",getB(side));
+    printf("объем равен %.2lf",getVolume(side));
+    printf("площадь поверхности равна %.2lf",getSquare(side));
 
     return 0;
 }
-double getA (const double side)
+double getVolume (const double side)
 {
     return 4/3 * M_PI * (double)pow(side, 3);
 }
-double getB (const double side)
+double getSquare (const double side)
 {
     return 4 * M_PI * (double)pow(side, 2);
 }
